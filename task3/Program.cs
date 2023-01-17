6deba8a90eae3d7f42f99c1e5d7a6d5b01dfaa0a
poliex <poliex86@gmail.com>
2023-01-17 21:41:21 +0500

    Task 3 complete

---
+++ task3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите номер четверти: ");
+var quarteNumber = Console.ReadLine() ?? "";
+switch (quarteNumber)
+    {
+        case "1":
+            Console.Write("Доступные координаты: X > 0, Y > 0");
+            break;
+        case "2":
+            Console.Write("Доступные координаты: X < 0, Y > 0");
+            break;
+        case "3":
+            Console.Write("Доступные координаты: X < 0, Y < 0");
+            break;
+        case "4":
+            Console.Write("Доступные координаты: X > 0, Y < 0");
+            break;
+        default:
+            Console.Write("Неверный ввод");
+            break;
+    }
