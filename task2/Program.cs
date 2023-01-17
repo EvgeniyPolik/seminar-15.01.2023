d37356224e442c901d03fdfe6c171bdd7c7a0c42
poliex <poliex86@gmail.com>
2023-01-17 21:06:05 +0500

    Task 2 complete

---
+++ task2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите первое число: ");
+var a = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите второе число: ");
+var b = int.Parse(Console.ReadLine() ?? "");
+if (a == b*b)
+    Console.Write($"Число {a} является квадратом числа {b}");
+else
+    Console.Write($"Число {a} не является квадратом числа {b}");
