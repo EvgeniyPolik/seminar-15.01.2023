f7d3a615e8188d339e0ea396a07b2aa4cc122224
poliex <poliex86@gmail.com>
2023-01-17 20:56:01 +0500

    Task 1 complete

---
+++ task1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите номер дня недели: ");
+var numberDay = int.Parse(Console.ReadLine() ?? "");
+if (numberDay > 0 && numberDay <= 7)
+    if (numberDay == 6 || numberDay == 7)
+        Console.WriteLine("Выбран выходной день");
+    else
+        Console.WriteLine("Выбран будний день");
+else
+     Console.WriteLine("Не верный ввод");
